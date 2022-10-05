e0ac6557ecdc0e1610b61a54fc801fb2eb31f409
Mohamed A. Eldeeb <meldeeb1998@gmail.com>
2022-10-05 21:56:46 +0200

    design footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Logo from "../images/logo.png";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Made with ♥️ and <b>React.js</b>
+      </span>
+    </footer>
+  );
 }
 
 export default Footer;
